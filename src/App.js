a9c81ab94c21d4e1aa3c2d76be6f9a141f2fddd9
ksamdata <ksamdata@gmail.com>
2022-09-30 01:41:55 +0100

    file update
    added react app source code

---
+++ src/App.js
@@ -0,0 +1,27 @@
+import logo from './logo.svg';
+import logo2 from './logo2.png';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+	<img src={logo2} className="App-logo2" alt="logo2" />
+        <p>
+          I just edited this in my reactapp
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
